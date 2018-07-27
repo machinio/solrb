d6a3d46b6aa000b6a940cc251185315630a94fdd
Adriano Luz <drnluz@gmail.com>
2018-07-27 12:05:58 -0300

    Make SolrCaller attributes public readable

--- lib/solr/solr_caller.rb
+++ lib/solr/solr_caller.rb
@@ -3,6 +3,8 @@ module Solr
     FORM_URLENCODED_CONTENT_TYPE = 'application/x-www-form-urlencoded; charset=UTF-8'.freeze
     SOLR_SELECT_PATH = 'select'.freeze
 
+    attr_reader :page, :page_size, :solr_params
+
     class << self
       def call(opts)
         new(opts).call
@@ -18,7 +20,7 @@ module Solr
     def call
       response = get_connection.post do |req|
         req.url(SOLR_SELECT_PATH)
-        req.body = @solr_params.merge({ wt: :json, rows: @page_size.to_i, start: page })
+        req.body = @solr_params.merge({ wt: :json, rows: page_size.to_i, start: get_page })
         req.headers['Content-Type'] = FORM_URLENCODED_CONTENT_TYPE
       end
 
@@ -27,7 +29,7 @@ module Solr
 
     private
 
-    def page
+    def get_page
       page = @page.to_i - 1
       page < 1 ? 0 : page
     end
