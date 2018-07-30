11f87e6d013757e47531924e8334586c38d6a0e3
Adriano Luz <drnluz@gmail.com>
2018-07-30 11:47:30 -0300

    Config solr field mapping function

--- lib/solr/schema_helper.rb
+++ lib/solr/schema_helper.rb
@@ -1,14 +1,7 @@
 module Solr
   module SchemaHelper
     def solarize_field(field)
-      solr_field = Solr.configuration.field_map[field]
-      # TODO looks very specific
-      return "#{solr_field}_#{I18n.locale}" if solr_field.to_s.ends_with?('_text')
-      solr_field
-    end
-
-    def desolarize_field(solr_field)
-      Solr.configuration.inverse_field_map[solr_field]
+      Solr.configuration.field_map_function.call(field)
     end
   end
 end
