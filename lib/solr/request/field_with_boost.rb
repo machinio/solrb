81c132a60eb7d318dffa04f1730138675339baeb
Adriano Luz <drnluz@gmail.com>
2018-08-01 09:36:34 -0300

    Remove locale from fields on FieldWithBoost

--- lib/solr/request/field_with_boost.rb
+++ lib/solr/request/field_with_boost.rb
@@ -9,9 +9,7 @@ module Solr
       end
 
       def to_solr_s
-        # TODO: This is very specific for machinio, make more generic
-        solr_s = field.to_s.sub('_text', "_text_#{I18n.locale}")
-        "#{solr_s}^#{boost_magnitude}"
+        "#{field}^#{boost_magnitude}"
       end
     end
   end
