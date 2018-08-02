f5a9ef6be3ffe26d874dd2fd0be2bcf2ce121348
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2018-08-02 17:12:38 +0300

    Specs

--- spec/indexing/indexing_spec.rb
+++ spec/indexing/indexing_spec.rb
@@ -1,9 +1,15 @@
 RSpec.describe Solr::Indexing do
-  it 'accepts a single document for indexing' do
-    doc = Solr::Indexing::Document.new
-    doc.add_field(:id, 994)
-    doc.add_field(:name, 'Solrb')
+  it 'indexes a single document' do
+    doc = Solr::Indexing::Document.new(id: 994, name: 'Solrb')
     req = Solr::Indexing::Request.new([doc], commit: true)
+    resp = req.run
+    expect(resp.status).to eq :OK
+  end
+
+  it 'indexes multiple documents' do
+    doc1 = Solr::Indexing::Document.new(id: 995, name: 'Curitiba')
+    doc2 = Solr::Indexing::Document.new(id: 996, name: 'Kislovodsk')
+    req = Solr::Indexing::Request.new([doc1, doc2], commit: true)
     resp = req.run
     expect(resp.status).to eq :OK
   end
