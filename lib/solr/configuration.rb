f81c69a93a0512c4000b3835dccdc1d52e31fb81
Adriano Luz <drnluz@gmail.com>
2018-08-03 12:36:54 -0300

    Raise error if solr url was not found

--- lib/solr/configuration.rb
+++ lib/solr/configuration.rb
@@ -3,9 +3,20 @@ require 'solr/field_configuration/dynamic_field'
 require 'solr/field_configuration/dynamic_field_definition_builder'
 require 'solr/field_configuration/dynamic_field_mapper'
 
+
 module Solr
   class Configuration
-    attr_accessor :faraday_options, :url, :field_types, :fields, :test_connection
+    NO_URL_ERROR_MESSAGE = '
+    Solrb gem requires you to set the URL of your Solr instance
+    either through SOLR_URL environmental variable or explicitly inside the configure block:
+
+    Solr.configure do |config|
+      config.url = "http://localhost:8983/solr/core"
+    end
+    '.freeze
+
+    attr_accessor :faraday_options, :field_types, :fields, :test_connection
+    attr_writer :url
 
     def initialize
       @faraday_options = { request: { timeout: 2, open_timeout: 8 } }
@@ -16,6 +27,11 @@ module Solr
 
     def uri
       @uri ||= URI.parse(url)
+    end
+
+    def url
+      raise NO_URL_ERROR_MESSAGE unless @url
+      @url
     end
 
     def define_dynamic_fields
