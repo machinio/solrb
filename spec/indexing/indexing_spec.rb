2bd5098f19ddc311c6d6b97adda3878d37917740
Adriano Luz <drnluz@gmail.com>
2018-08-08 12:34:34 -0300

    Move code to before and after blocks on IndexingSpec

--- spec/indexing/indexing_spec.rb
+++ spec/indexing/indexing_spec.rb
@@ -1,6 +1,5 @@
 RSpec.describe Solr::Indexing do
   context 'without configuration' do
-
     it 'indexes a single document' do
       doc = Solr::Indexing::Document.new(id: 994, name_txt_en: 'Solrb')
       req = Solr::Indexing::Request.new([doc])
@@ -17,21 +16,28 @@ RSpec.describe Solr::Indexing do
       expect(resp.status).to eq 'OK'
     end
   end
+
   context 'with configuration' do
-    it 'indexes with dynamic field configuration' do
+    before do
       Solr.configure do |config|
         config.define_fields do |f|
           f.field :title, dynamic_field: :text_en
           f.dynamic_field :text_en, solr_name: '*_txt_en'
         end
       end
+    end
+
+    after do
+      # return default configuration
+      Solr.configuration = Solr::Configuration.new
+    end
+
+    it 'indexes with dynamic field configuration' do
       doc1 = Solr::Indexing::Document.new(id: 10, title: 'iPhone X')
       req = Solr::Indexing::Request.new([doc1])
       resp = req.run(commit: true)
       puts resp.inspect unless resp.ok?
       expect(resp.status).to eq 'OK'
-      # return default configuration
-      Solr.configuration = Solr:: Configuration.new
     end
   end
 end
