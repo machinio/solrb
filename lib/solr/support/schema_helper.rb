574d24d20ddd685def86eae1930cd55dbd9d4d6e
Vladislav Syabruk <sjabrik@gmail.com>
2018-08-22 18:02:26 +0300

    Improve solarize_field method

--- lib/solr/support/schema_helper.rb
+++ lib/solr/support/schema_helper.rb
@@ -5,11 +5,7 @@ module Solr
         core = Solr.configuration.cores[core_name.to_sym] if core_name
         core ||= Solr.configuration.default_core if Solr.configuration.cores.count == 1
         field_config = core.field_by_name(field.to_sym) if core
-        if field_config
-          field_config.solr_field_name
-        else
-          field.to_s
-        end
+        field_config ? field_config.solr_field_name : field.to_s
       end
     end
   end
