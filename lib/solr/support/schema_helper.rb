bf0477c4ccda2f141f78650eda740c52a4e5d7fc
Vladislav Syabruk <sjabrik@gmail.com>
2018-08-22 18:17:47 +0300

    Simplify solarize_field method

--- lib/solr/support/schema_helper.rb
+++ lib/solr/support/schema_helper.rb
@@ -2,10 +2,17 @@ module Solr
   module Support
     module SchemaHelper
       def solarize_field(core_name: nil, field:)
-        core = Solr.configuration.cores[core_name.to_sym] if core_name
-        core ||= Solr.configuration.default_core if Solr.configuration.cores.count == 1
+        core = core_by_core_name(core_name)
         field_config = core.field_by_name(field.to_sym) if core
         field_config ? field_config.solr_field_name : field.to_s
+      end
+
+      private
+
+      def core_by_core_name(core_name)
+        core = Solr.configuration.cores[core_name.to_sym] if core_name
+        core ||= Solr.configuration.default_core if Solr.configuration.cores.count == 1
+        core
       end
     end
   end
