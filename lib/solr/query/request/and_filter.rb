35b8ecffc39919023ff201cc5dddebed02fad283
Vladislav Syabruk <sjabrik@gmail.com>
2018-10-04 12:16:10 +0300

    Add Solr::Query::Request::AndFilter

--- lib/solr/query/request/and_filter.rb
+++ lib/solr/query/request/and_filter.rb
@@ -1,14 +1,14 @@
 module Solr
   module Query
     class Request
-      class OrFilter
+      class AndFilter
         def initialize(*filters)
           @filters = filters
         end
 
         def to_solr_s
           subexpressions = @filters.map(&:to_solr_s)
-          "(#{subexpressions.join(' OR ')})"
+          "(#{subexpressions.join(' AND ')})"
         end
       end
     end
