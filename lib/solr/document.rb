bb4ce781aead51e7b531b82be2e90efa8f4505a0
Vladislav Syabruk <sjabrik@gmail.com>
2019-01-24 21:03:00 +0300

    Rename attributes to fields

--- lib/solr/document.rb
+++ lib/solr/document.rb
@@ -12,16 +12,16 @@ module Solr
       end
     end
 
-    attr_reader :id, :model_name, :score, :debug_info, :group, :attributes
+    attr_reader :id, :model_name, :score, :debug_info, :group, :fields
 
     # TODO: model_name is specific for machinio
-    def initialize(id:, model_name: nil, score: nil, debug_info: nil, group: GroupInformation.empty, attributes: {})
+    def initialize(id:, model_name: nil, score: nil, debug_info: nil, group: GroupInformation.empty, fields: {})
       @id = id
       @model_name = model_name
       @score = score
       @debug_info = debug_info
       @group = group
-      @attributes = attributes
+      @fields = fields
     end
   end
 end
