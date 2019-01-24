9ae6b8ab53bccbdd204825491d99d5edbace4669
Vladislav Syabruk <sjabrik@gmail.com>
2019-01-24 22:48:40 +0300

    Fix specs

--- lib/solr/query/request/edismax_adapter.rb
+++ lib/solr/query/request/edismax_adapter.rb
@@ -43,19 +43,19 @@ module Solr
 
         def add_query_fields(solr_params)
           query_fields = request.query_fields.map(&:to_solr_s)
-          solr_params.merge(EDISMAX_QUERY_FIELDS => query_fields)
+          solr_params[EDISMAX_QUERY_FIELDS] = query_fields
+          solr_params
         end
 
         def add_filters(solr_params)
           filters = request.filters.map(&:to_solr_s)
+          solr_params[FILTER_QUERY] = filters
+          solr_params
         end
         
         def add_facets(solr_params)
           return solr_params if Array(request.facets).empty?
-          solr_params.merge(FILTER_QUERY => filters)
-
           solr_params[JSON_FACET] = request.facets.map(&:to_solr_h).reduce(&:merge).to_json
-
           solr_params
         end
 
@@ -69,16 +69,15 @@ module Solr
         def add_additive_boost_functions(solr_params)
           additive_boosts = request.boosting.additive_boost_functions.map(&:to_solr_s)
           if additive_boosts.any?
-            solr_params.merge(EDISMAX_ADDITIVE_BOOST_FUNCTION => additive_boosts)
-          else
-            solr_params
+            solr_params[EDISMAX_ADDITIVE_BOOST_FUNCTION] = additive_boosts
           end
+          solr_params
         end
 
         def add_multiplicative_boost_functions(solr_params)
           multiplicative_boosts = request.boosting.multiplicative_boost_functions.map(&:to_solr_s)
           if multiplicative_boosts.any?
-            solr_params = solr_params.merge(EDISMAX_MULTIPLICATIVE_BOOST_FUNCTION => multiplicative_boosts)
+            solr_params[EDISMAX_MULTIPLICATIVE_BOOST_FUNCTION] = multiplicative_boosts
             # https://stackoverflow.com/questions/47025453/
             maybe_add_spatial_fields(solr_params, request.boosting.spatial_boost)
           else
@@ -89,18 +88,16 @@ module Solr
         def add_phrase_boosts(solr_params)
           solr_phrase_boosts = request.boosting.phrase_boosts.map(&:to_solr_s)
           if solr_phrase_boosts.any?
-            solr_params.merge(EDISMAX_PHRASE_BOOST => solr_phrase_boosts)
-          else
-            solr_params
+            solr_params[EDISMAX_PHRASE_BOOST] = solr_phrase_boosts
           end
+          solr_params
         end
 
         def maybe_add_spatial_fields(solr_params, geodist_function)
           if geodist_function
-            solr_params.merge(pt: geodist_function.latlng, sfield: geodist_function.sfield)
-          else
-            solr_params
+            solr_params.merge!(pt: geodist_function.latlng, sfield: geodist_function.sfield)
           end
+          solr_params
         end
 
         def add_grouping(solr_params)
@@ -117,17 +114,20 @@ module Solr
         def add_sorting(solr_params)
           return solr_params if request.sorting.empty?
           solr_sorting = request.sorting.fields.map(&:to_solr_s).join(', ')
-          solr_params.merge(sort: solr_sorting)
+          solr_params[:sort] = solr_sorting
+          solr_params
         end
 
         def add_response_fields(solr_params)
           request.response_fields.fields << :score if debug_mode?
           response_fields = request.response_fields.to_solr_s
-          solr_params.merge(RESPONSE_FIELDS => response_fields)
+          solr_params[RESPONSE_FIELDS] = response_fields
+          solr_params
         end
 
         def add_debug(solr_params)
-          solr_params.merge(debug: debug_mode?)
+          solr_params[:debug] = debug_mode?
+          solr_params
         end
 
         def debug_mode?
@@ -141,17 +141,20 @@ module Solr
         def add_rerank_query(solr_params)
           return solr_params unless request.limit_docs_by_field
           rerank_query = request.limit_docs_by_field.to_solr_s
-          solr_params.merge(RERANK_QUERY => rerank_query)
+          solr_params[RERANK_QUERY] = rerank_query
+          solr_params
         end
 
         def add_phrase_slop(solr_params)
           return solr_params unless request.phrase_slop
-          solr_params.merge(EDISMAX_PHRASE_SLOP => request.phrase_slop)
+          solr_params[EDISMAX_PHRASE_SLOP] = request.phrase_slop
+          solr_params
         end
 
         def add_query_operator(solr_params)
           return solr_params unless request.query_operator
-          solr_params.merge(QUERY_OPERATOR => request.query_operator)
+          solr_params[QUERY_OPERATOR] = request.query_operator
+          solr_params
         end
       end
     end
