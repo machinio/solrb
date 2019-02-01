0cfbc3857887ef3563a51f80c83fc2be3fe7977e
Adriano Luz <drnluz@gmail.com>
2019-02-01 15:10:44 -0200

    Simplify Solr::Query::Request::Runner interface

--- lib/solr/query/request/runner.rb
+++ lib/solr/query/request/runner.rb
@@ -6,24 +6,26 @@ module Solr
 
         include Solr::Support::ConnectionHelper
 
-        attr_reader :page, :page_size, :offset, :rows, :solr_params
+        attr_reader :start, :rows, :solr_params
 
         class << self
           def run(opts)
             new(opts).run
           end
+
+          def run_paged(page: 1, page_size: 10, solr_params: {})
+            start_page = page.to_i - 1
+            start_page = start_page < 1 ? 0 : start_page
+            start = start_page * page_size
+
+            new(start: start, rows: page_size, solr_params: solr_params).run
+          end
         end
 
-        def initialize(page: nil, page_size: nil, rows: nil, offset: nil, solr_params: {})
-          @page = page
-          @page_size = page_size
-          @offset = offset
+        def initialize(rows: nil, start: nil, solr_params: {})
+          @start = start
           @rows = rows
           @solr_params = solr_params
-
-          if !((page && page_size) || (rows && offset))
-            raise ArgumentError.new("Expected one of the following argument pairs: (page, page_size) or (rows, offset)")
-          end
         end
 
         def run
@@ -34,22 +36,9 @@ module Solr
 
         private
 
-        def start
-          return offset.to_i if offset
-
-          start_page = @page.to_i - 1
-          start_page = start_page < 1 ? 0 : start_page
-          start_page * page_size
-        end
-
         def request_params
           # https://lucene.apache.org/solr/guide/7_1/json-request-api.html#passing-parameters-via-json
-          @request_params ||= { params: solr_params.merge(wt: :json, rows: row_count, start: start) }
-        end
-
-        def row_count
-          return rows.to_i if rows
-          page_size.to_i
+          @request_params ||= { params: solr_params.merge(wt: :json, rows: rows, start: start) }
         end
       end
     end
