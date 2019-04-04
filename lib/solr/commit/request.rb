5cb7b999fda248bc918bce4bbcea18e95c515e14
Dmitry Vorotilin <d.vorotilin@gmail.com>
2019-04-04 13:48:57 +0300

    Indexing through leader node

--- lib/solr/commit/request.rb
+++ lib/solr/commit/request.rb
@@ -3,9 +3,16 @@ module Solr
     class Request
       PATH = '/update'.freeze
 
-      def run
+      def run(runner_options: nil)
         http_request = Solr::Request::HttpRequest.new(path: PATH, url_params: { commit: true }, method: :post)
-        Solr::Request::Runner.call(request: http_request)
+        options = default_runner_options.merge(runner_options || {})
+        Solr::Request::Runner.call(request: http_request, **options)
+      end
+
+      private
+
+      def default_runner_options
+        { node_selection_strategy: Solr::Request::LeaderNodeSelectionStrategy }
       end
     end
   end
