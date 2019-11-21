df3845063a2fe57b4953bd1fd3b2dd24727b2316
Viktor Sheiko <vv.sheiko@yandex.ru>
2019-11-21 13:59:19 +0300

    Added an option to pass a default_boost to Boosting::DictionaryBoostFunction

--- lib/solr/query/request/boosting/dictionary_boost_function.rb
+++ lib/solr/query/request/boosting/dictionary_boost_function.rb
@@ -5,12 +5,13 @@ module Solr
         class DictionaryBoostFunction
           include Solr::Support::SchemaHelper
           using Solr::Support::StringExtensions
-          attr_reader :field, :dictionary
+          attr_reader :field, :dictionary, :default_boost
 
-          def initialize(field:, dictionary:)
+          def initialize(field:, dictionary:, default_boost: 1)
             raise 'dictionary must be a non-empty Hash' if Hash(dictionary).empty?
             @field = field
             @dictionary = dictionary
+            @default_boost = default_boost
           end
 
           # example: given a hash (dictionary)
@@ -21,7 +22,7 @@ module Solr
           # note that I added spaces for readability, real Solr query functions must always be w/out spaces
           def to_solr_s
             sf = solarize_field(field)
-            dictionary.to_a.reverse.reduce(1) do |acc, (field_value, boost)|
+            dictionary.to_a.reverse.reduce(default_boost) do |acc, (field_value, boost)|
               if field_value.is_a?(String) || field_value.is_a?(Symbol)
                 "if(termfreq(#{sf},\"#{field_value.to_s.solr_escape}\"),#{boost},#{acc})"
               else
