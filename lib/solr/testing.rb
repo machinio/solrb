cc9ff3fe33ec88dd9c18f2e2c6c34e7670766a29
Vladislav Syabruk <vlad@syabruk.com>
2022-08-25 17:44:16 +0400

    Change Solr::Testing.last_solr_request_params to Solr::Testing.last_solr_request

--- lib/solr/testing.rb
+++ lib/solr/testing.rb
@@ -1,7 +1,7 @@
 module Solr
   module Testing
     class << self
-      attr_accessor :last_solr_request_params
+      attr_accessor :last_solr_request
     end
   end
 end
@@ -9,7 +9,7 @@ end
 module Solr::Request::RunnerExtension
   def call
     response = super
-    Solr::Testing.last_solr_request_params = request.body ? request.body[:params] : nil
+    Solr::Testing.last_solr_request = request
     response
   end
 end
