c86a42a369d9535929fe4c8104af026c9d780ce3
Vladislav Syabruk <vlad@syabruk.com>
2024-01-16 18:15:45 +0300

    Fix spec

--- spec/query/request/geofilt_spec.rb
+++ spec/query/request/geofilt_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Solr::Query::Request::Geofilt do
   describe '.to_solr_s' do
-    let(:spatial_point) { Solr::SpatialPoint.new(latitude: -25.429692, longitude: -49.271265) }
+    let(:spatial_point) { Solr::SpatialPoint.new(lat: -25.429692, lon: -49.271265) }
 
     subject { described_class.new(field: :machine_type, spatial_point: spatial_point, spatial_radius: 161) }
 
